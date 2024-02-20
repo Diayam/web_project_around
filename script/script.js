21ea6aeda08d431893543c120b0346199e66be70
diana <d-y-m-a@hotmail.com>
2024-02-20 15:15:13 -0600

    popup edit

--- script/script.js
+++ script/script.js
@@ -1,22 +1,33 @@
-const profileContentElelement =document.querySelector(".prefile__content");
+const profileButtonEdit = document.querySelector(".profile__button-edit");
+const popupCloseButton = document.querySelector(".popup__close-button");
+const popup = document.querySelector (".popup");
+
+function openpopup() {
+    popup.classList.add("popup__open");
+}
+function closspopup() {
+    popup.classList.remove("popup__open");
+}
+
+profileButtonEdit.addEventListener("click",openpopup);
+popupCloseButton.addEventListener("click" , closspopup);
+
+
+
+
+
+
+/*const profileContentElelement =document.querySelector(".prefile__content");
 const profileNameElement = document.querySelector(".profile__name");
 const profileSubnameElement = document.querySelector(".profile__subname");
-
-const editProfileButton = document.querySelector(".profile__Button-edit");
-
 const profileButtonAdd = document.querySelector(".profile__button-add");
-const popupElemente = document.querySelector (".popup");
+
 const popuInputName = document.querySelector(".popup__input_name");
 const popupInputAbout = document.querySelector(".popup__input_about");
 const popupSaveButton = document.querySelector(".popup__save-button");
 const popupCloseButton = document.querySelector(".popup__close-button");
-
-editProfileButton.editProfileButton
-
-editProfileButton.addEventListener("click", popupopen);
-
-
-/*const profileName = profileNameElement.textContent;
+const profileName = profileNameElement.textContent;
 const profileSubname = profileSubnameElement.textContent;
 console.log(profileName);
-console.log(profileSubname);*/
+console.log(profileSubname);
+*/
