9b95366f05a43f1e867242b1b9a9b7ccc5e2fff9
diana <d-y-m-a@hotmail.com>
2024-03-28 10:49:37 -0500

    boton para cerra nwe place listo

--- script/script.js
+++ script/script.js
@@ -7,9 +7,9 @@ const popuInputName = document.querySelector(".popup__input_name");
 const popupInputAbout = document.querySelector(".popup__input_about");
 const profileForm = document.querySelector(".popup__edit");
 
-const editForm = document.querySelector("#edit__add");
+const editForm = document.querySelector("#place__add");
 const buttonEditAdd = document.querySelector(".profile__button-add");
-
+const buttonplace = document.querySelector("#place__close");
 
 popuInputName.value = profileNameElement.textContent;
 popupInputAbout.value = profileSubnameElement.textContent;
@@ -29,10 +29,13 @@ function save(event) {
 function openEdit() {
   editForm.classList.add("popup__open");
 }
-
+function clossPopup2() {
+  editForm.classList.remove("popup__open");
+}
 
 profileButtonEdit.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", clossPopup);
 profileForm.addEventListener("submit", save);
 
 buttonEditAdd.addEventListener("click", openEdit);
+buttonplace.addEventListener("click", clossPopup2);
